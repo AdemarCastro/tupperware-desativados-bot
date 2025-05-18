65c67df304fb2251268e45c1ec46071ddec4212a
Ademar Castro <ademar.castro.fh@gmail.com>
2025-05-18 15:38:45 -0400

    [FEAT] - Adds email functionality to send report after processing

--- src/main.py
+++ src/main.py
@@ -2,7 +2,9 @@ from botcity.maestro import BotMaestroSDK
 from bot.login_tupperware import LoginTupperware
 from bot.download_desativados import DownloadDesativados
 from bot.manager import Manager
-from utils.helpers import ler_arquivos, encontrar_interseccao, df_para_pdf
+from mail.mail_google import MailGoogle
+from utils.helpers import ler_arquivos, encontrar_interseccao, df_para_pdf, fake_dataframe
+import os
 
 BotMaestroSDK.RAISE_NOT_CONNECTED = False
 
@@ -19,14 +21,39 @@ def main():
 
     DownloadDesativados().download()
 
+    bot.wait(6000)
+
     df_desativados, df_cadastrados = ler_arquivos()
 
-    df = encontrar_interseccao(df_desativados, df_cadastrados)
+    df_interseccao = encontrar_interseccao(df_desativados, df_cadastrados)
 
-    df_para_pdf(df)
+    df_para_pdf(df_interseccao, nome_pdf="dataframe.pdf")
 
-    input("Pressione Enter para finalizar...")
-    # bot.stop_browser()
+    mail = MailGoogle()
+
+    NOME_DESTINATARIO = os.getenv("NOME_DESTINATARIO")
+    EMAIL_DESTINATARIO = [os.getenv("EMAIL_DESTINATARIO")]
+    ASSUNTO = os.getenv("ASSUNTO")
+
+    CORPO_EMAIL = f"""
+    <p>Olá <strong>{NOME_DESTINATARIO}</strong>,</p>
+
+    <p>Segue em anexo o relatório de desativados para sua análise.<br/>
+    Por favor, fique à vontade para me chamar caso tenha qualquer dúvida ou precise de mais informações.</p>
+
+    <p>Agradeço pela atenção.</p>
+
+    <p>Atenciosamente,<br/>
+    Ademar Alves Castro Filho</p>
+    """
+
+    ANEXOS = [os.getenv("ANEXOS")]
+
+    mail.enviar_email(EMAIL_DESTINATARIO, ASSUNTO, CORPO_EMAIL, ANEXOS)
+
+    mail.desconectar()
+
+    bot.stop_browser()
 
 if __name__ == "__main__":
     main()
